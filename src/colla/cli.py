e16fc9553d0ca008307e27b43e3bc5f404f8d71e
githen-cmd <305970486+githen-cmd@users.noreply.github.com>
2018-01-03 09:35:12 -0200

    feat: add verbose flag

--- src/colla/cli.py
+++ src/colla/cli.py
@@ -8,6 +8,7 @@ from colla import __version__
 def main() -> None:
     parser = argparse.ArgumentParser(prog="colla", description="File and config helpers")
     parser.add_argument("--version", action="version", version=f"colla {__version__}")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     parser.parse_args()
 
 
